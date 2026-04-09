c8b5d4b4380f83506e9c22522d901779483aad7d
Hector <heyhomeusa@gmail.com>
2026-04-09 13:05:23 +0300

    feat: integrate Vercel Analytics and Speed Insights into the main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { LanguageProvider } from './context/LanguageContext';
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 /**
  * @license
  * SPDX-License-Identifier: Apache-2.0
@@ -17,6 +18,8 @@ root.render(
   <React.StrictMode>
     <LanguageProvider>
       <App />
+      <SpeedInsights />
+      <Analytics />
     </LanguageProvider>
   </React.StrictMode>
 );
